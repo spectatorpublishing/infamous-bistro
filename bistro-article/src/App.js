5795e21e6067753b2656763b31f95a6b207fa976
Sophia Yao <sy2743@columbia.edu>
2019-04-09 20:44:44 -0400

    title page

--- bistro-article/src/App.js
+++ bistro-article/src/App.js
@@ -1,11 +1,54 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const ShortTitle = styled.div`
+  font-size : 1.0rem;
+  padding: 0.5rem;
+  margin-left: auto;
+  margin-right: auto;
+`
+const LongTitle = styled.div`
+  font-size: 3.0rem;
+  width: 60vw;
+  padding: 0.5rem;
+  margin-left: auto;
+  margin-right: auto;
+`
+const Description = styled.div`
+  font-size: 1.5rem;
+  width: 40vw;
+  margin-left: auto;
+  margin-right: auto;
+`
+const Titles = styled.div`
+  padding: 10.0rem;
+  text-align: center;
+`
+
+const Circle = styled.div`
+  padding: 2.0rem;
+  text-align: center;
+`
+const content = styled.div`
+  padding: 10.0rem;
+  text-align: center;
+`
 
 class App extends Component {
+  
+  onClickButton(){
+    window.scrollTo(0, 1000);
+  }
+
   render() {
     return (
-      <div className="App">
-
-      </div>
+      <Titles>
+        <ShortTitle>HARMONY. MASTERED FROM CHAOS.</ShortTitle>
+        <LongTitle>In Miami, a Chef Sources Locally for His South Asian Desserts</LongTitle>
+        <Description>Florida’s tropical monsoon climate helps Niven Patel master the sweet side of his modern Indian menu.</Description>
+        <Circle><img src="https://upload.wikimedia.org/wikipedia/commons/a/a0/Circle_-_black_simple.svg" alt="Logo" width={50} padding={100} onClick={this.onClickButton}/></Circle>
+        <div ref={this.myRef}>hiiiiii</div> >
+      </Titles>
     );
   }
 }
