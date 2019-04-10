28b63712c52d5cd84787e9cd9d9520739d919154
Rounak Bera <rounakbera@gmail.com>
2019-04-09 21:42:48 -0400

    global fonts

---
+++ bistro-article/src/global-styles.js
@@ -0,0 +1,16 @@
+import { injectGlobal } from 'styled-components';
+
+injectGlobal`
+  @font-face {
+     font-family: 'redgar';
+     src: url('assets/Redgar..ttf');
+  }
+@font-face {
+    font-family: 'merriweatherlight';
+    src: url('assets/MerriweatherSans-Light.otf')
+}
+@font-face {
+    font-family: 'merriweatherbold';
+    src: url('assets/MerriweatherSans-Bold.otf')
+}
+`
