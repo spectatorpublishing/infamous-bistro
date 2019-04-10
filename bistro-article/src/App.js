facc495b60d864648038eedbd6eb36a63c3024af
William Chiu <williamchiu16@gmail.com>
2019-04-09 22:04:34 -0400

    implement slides and background

--- bistro-article/src/App.js
+++ bistro-article/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import Slide from './Components/Slide';
+import Background from './Components/Background';
 
 const ShortTitle = styled.div`
   font-size : 1.0rem;
@@ -34,14 +36,33 @@ const content = styled.div`
   text-align: center;
 `
 
+const Backgrounds = [
+  "https://images.pexels.com/photos/356378/pexels-photo-356378.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  "https://cdn.pixabay.com/photo/2019/02/16/01/18/dog-3999619_1280.jpg",
+  "https://c.pxhere.com/photos/b9/d8/dogs_puppies_play_two_group_in_the_free_nature_pet-652302.jpg!d",
+  "http://www.dodlive.mil/files/2013/03/829140.jpg"
+]
+
 class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      background: null
+    }
+    this.updateBackground = this.updateBackground.bind(this)
+  }
   
   onClickButton(){
     window.scrollTo(0, 1000);
   }
 
+  updateBackground(i) {
+    this.setState({background: i})
+  }
+
   render() {
-    return (
+    /*return (
       <Titles>
         <ShortTitle>HARMONY. MASTERED FROM CHAOS.</ShortTitle>
         <LongTitle>In Miami, a Chef Sources Locally for His South Asian Desserts</LongTitle>
@@ -50,6 +71,15 @@ class App extends Component {
         <div ref={this.myRef}>hiiiiii</div> >
       </Titles>
     );
+    */
+    return <div>
+      <Background background={this.state.background}/>
+      <div>
+      {
+        Backgrounds.map(i => <Slide background={i} updateBackground={this.updateBackground}/>)
+      }
+      </div>
+    </div>
   }
 }
 
