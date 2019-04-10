f3b51574cf38eb70f9920e0087c2ab68083cccbd
Kenevian <Katon.vanetten@gmail.com>
2019-04-09 21:37:38 -0400

    Working On Question

--- bistro-article/src/Question.js
+++ bistro-article/src/Question.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
-const question = styled.div`
+const Prompt = styled.div`
    font-size: 1.0rem;
    font-family: regar;
 `
+
+class Question extends Component {
+   render() {
+      return (
+         <Prompt></Prompt>
+      )
+   }
+}
