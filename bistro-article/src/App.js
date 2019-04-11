195e42b85c1d9bca48bd1c6eca565ac75138f844
Rounak Bera <rounakbera@gmail.com>
2019-04-10 21:52:56 -0400

    deleted titles from app.js

--- bistro-article/src/App.js
+++ bistro-article/src/App.js
@@ -23,39 +23,6 @@ const GlobalStyle = createGlobalStyle`
   }
 `
 
-const ShortTitle = styled.div`
-  font-size : 1.0rem;
-  padding: 0.5rem;
-  margin-left: auto;
-  margin-right: auto;
-`
-const LongTitle = styled.div`
-  font-size: 3.0rem;
-  width: 60vw;
-  padding: 0.5rem;
-  margin-left: auto;
-  margin-right: auto;
-`
-const Description = styled.div`
-  font-size: 1.5rem;
-  width: 40vw;
-  margin-left: auto;
-  margin-right: auto;
-`
-const Titles = styled.div`
-  padding: 10.0rem;
-  text-align: center;
-`
-
-const Circle = styled.div`
-  padding: 2.0rem;
-  text-align: center;
-`
-const content = styled.div`
-  padding: 10.0rem;
-  text-align: center;
-`
-
 const Backgrounds = [
   "https://images.pexels.com/photos/356378/pexels-photo-356378.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
   "https://cdn.pixabay.com/photo/2019/02/16/01/18/dog-3999619_1280.jpg",
@@ -82,17 +49,6 @@ class App extends Component {
   }
 
   render() {
-    /*return (
-      <Titles>
-        <ShortTitle>HARMONY. MASTERED FROM CHAOS.</ShortTitle>
-        <LongTitle>In Miami, a Chef Sources Locally for His South Asian Desserts</LongTitle>
-        <Description>Florida’s tropical monsoon climate helps Niven Patel master the sweet side of his modern Indian menu.</Description>
-        <Circle><img src="https://upload.wikimedia.org/wikipedia/commons/a/a0/Circle_-_black_simple.svg" alt="Logo" width={50} padding={100} onClick={this.onClickButton}/></Circle>
-        <div ref={this.myRef}>hiiiiii</div> >
-      </Titles>
-
-    );
-    */
     return <div>
       <GlobalStyle/>
       <Background background={this.state.background}/>
